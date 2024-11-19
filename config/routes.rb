7044d475964a09d5a187ec1615479eb0d40b3287
Sebastiao Trindade <sebatrindade@hotmail.com>
2024-11-19 12:03:42 -0300

    Adicionando página alarme

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'timers#digital'
   get 'timers/digital', to: 'timers#digital'
   get 'timers/clock', to: 'timers#clock'
+  get 'timers/alarm', to: 'timers#alarm'
   
 end
