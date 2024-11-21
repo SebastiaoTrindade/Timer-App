324120ec09e9c1dff487fd7ef10a01dd280e205b
Sebastiao Trindade <sebatrindade@hotmail.com>
2024-11-21 11:39:46 -0300

    Ajustes de rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'timers#digital'
-  get 'timers/digital', to: 'timers#digital'
-  get 'timers/clock', to: 'timers#clock'
-  get 'timers/alarm', to: 'timers#alarm'
+  root to: 'timers#digital'  
+  get 'clock', to: 'timers#clock'
+  get 'alarm', to: 'timers#alarm'
   
 end
